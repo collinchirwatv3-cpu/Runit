9f45a9593e89f7dcf983496ed54e02d83ec7f381
collinchirwatv3-cpu <collinchirwatv3@gmail.com>
2026-05-31 09:25:14 +0200

    feat: add Sentry error monitoring

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from 'expo';
+import * as Sentry from '@sentry/react-native';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+Sentry.init({
+  dsn: 'https://368904ba3b2be1d306d79049c8e466a2@o4511483258273792.ingest.de.sentry.io/4511483297136720',
+  tracesSampleRate: 1.0,
+  environment: __DEV__ ? 'development' : 'production',
+});
+
 registerRootComponent(App);
